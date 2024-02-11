15459f5647ceae2683e436d9b946c68300fda701
Md Sagor Islam <sagorislam1318@gmail.com>
2024-02-11 20:11:41 +0600

    put method done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 //For env File
 require("dotenv").config();
-import { Request, Response } from "express";
-import { ObjectId } from "mongodb";
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -11,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { ObjectId, MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gaxw2ro.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,31 +28,29 @@ async function run() {
 
     const todosCollection = client.db("reduxTodoApp").collection("todos");
 
-    app.get("/todos", async (req: Request, res: Response) => {
-      try {
-        const result = await todosCollection.find().toArray();
-        res.send(result);
-      } catch (error) {
-        res.status(500).send({ error: "Server Error." });
-      }
-    });
-    app.post("/todo", async (req: Request, res: Response) => {
+    // app.get("/todos", async (req: Request, res: Response) => {
+    //   try {
+    //     const result = await todosCollection.find().toArray();
+    //     res.send(result);
+    //   } catch (error) {
+    //     res.status(500).send({ error: "Server Error." });
+    //   }
+    // });
+    app.post("/todo", async (req, res) => {
       const todo = req.body;
       const result = await todosCollection.insertOne(todo);
       res.send(result);
     });
-    app.get("/todos", async (req: Request, res: Response) => {
-      // let query: { priority?: string | undefine };
-      let query: { [key: string]: any } = {};
-      // let query = {};
+    app.get("/todos", async (req, res) => {
+      let query = {};
       if (req.query.priority) {
         query.priority = req.query.priority;
       }
       const cursor = await todosCollection.find(query);
-      const todos = await cursor.toArray();
-      res.send({ status: true, data: todos });
+      const result = await cursor.toArray();
+      res.send(result);
     });
-    app.put("/todo/:id", async (req: Request, res: Response) => {
+    app.put("/todo/:id", async (req, res) => {
       const id = req.params.id;
       const todo = req.body;
       const filter = { _id: new ObjectId(id) };
@@ -87,7 +83,7 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
   res.send("Welcome to redux Todo application Server");
 });
 
